a695c86cad7b6fd36565491365308e6fc0141986
end-dot <aj.1803160@gmail.com>
2024-01-28 00:11:11 +0600

    String

---
+++ Strings.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Strings {
+    public static void main(String[] args) {
+        String name1="Jim";
+        String name2="Jim";
+        //compare two string
+        if(name1.compareTo(name2)==0){
+            System.out.println("Strings are equal");
+        }
+        else{
+            System.out.println("Not equal");
+        }
+
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s);
+        for(int i=0;i<s.length();i++){
+            System.out.print(s.charAt(i));
+        }
+        System.out.println();
+        String str=s.substring(0,3);
+        System.out.println(str);
+        
+    }
+    
+}
